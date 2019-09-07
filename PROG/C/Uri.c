6501f4c69b12fa1fbe6cf3f73780449bfd495c87
Raniel Evan <RanielEvann@gmail.com>
2019-09-06 21:34:20 -0300

    Create Uri.c

---
+++ PROG/C/Uri.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	
+	double pi = 3.14159;
+	double raio;
+	double area = pi * (raio * raio);
+	float total
+	
+	
+	scanf("%f %f", &qtd, &valor);
+	total = valor * qtd;
+	
+	printf("%.2f \nO valor digitado foi %.2f e a quantidade foi %d", total, valor, qtd);
+	
+	
+	return 0;
+	
+}
